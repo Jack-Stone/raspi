a2287217d0090df63ccb639e43a0ec169b77432f
Jack_Stone <jsitong@yahoo.com>
2017-03-11 21:13:55 +0800

    wrote a tweet track file

---
+++ tweet_test.py
@@ -0,0 +1,40 @@
+from twython import TwythonStreamer
+def count():
+	global n
+	n = n+1
+	if ( n > 2 ):
+		print "Ian G. Harris is popular!"
+				
+		
+class MyStreamer(TwythonStreamer):
+
+	def on_success(self, data):
+		if 'text' in data:
+			print("Found it.")
+			count()
+			print data['text'].encode('utf-8')
+
+
+
+C_K = raw_input('Enter consumer key:')
+C_S = raw_input('Enter consumer secret:')
+A_K = raw_input('Enter access token key:')
+A_S = raw_input('Enter access token secret:')
+
+n = 0
+
+try:
+	stream = MyStreamer(C_K, C_S, A_K, A_S)
+	stream.statuses.filter(track = "Ian G. Harris")
+	
+except KeyboardInterrupt:
+	exit()
+
+
+
+
+
+
+
+
+
